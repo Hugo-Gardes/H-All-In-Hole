d9e9c67524415fc5a2d52dfac5b3a2eeef2fc2d2
hugo gardes <hugo.gardes@epitech.eu>
2025-10-20 15:20:43 +0200

    chore: initial scene

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    public InputActionReference moveAction;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
